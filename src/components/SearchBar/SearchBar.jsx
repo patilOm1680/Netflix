84b7ad0fdb92259fa51d04121c21ecd33433b86b
Om Patil <patil4om@gmail.com>
2025-12-02 18:13:58 +0530

    Changed Search Functionality

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -10,9 +10,9 @@ export default function SearchBar() {
         // setTimeout(() => {
         //   handleSearchClick()
         // }, 2000)
-        
+
     }
-    
+
     const navigate = useNavigate();
 
     const structureInput = (input) => {
@@ -20,7 +20,7 @@ export default function SearchBar() {
     }
 
     useEffect(() => {
-        
+
         const searchInput = structureInput(input);
         const url = `${import.meta.env.VITE_SearchUrl}?api_key=${import.meta.env.VITE_ApiKey}&query=${searchInput}`
         axios.get(url)
@@ -31,13 +31,13 @@ export default function SearchBar() {
 
     }, [input]);
 
-    
+
     const handleSearchClick = () => {
         if (searchData.length > 0) {
             navigate("/search-result", { state: { Data: searchData } })
             // console.log(searchData)
-        }else{
-            navigate("/search-not-found",{state:{input}})
+        } else {
+            navigate("/search-not-found", { state: { input } })
         }
         // console.log("clicked search...")
     }
@@ -45,8 +45,8 @@ export default function SearchBar() {
 
     return (
         <>
-            <div>
-                <div class="flex items-center border border-gray-300 rounded-md px-1 relative">
+            <div className="relative">
+                {/* <div class="flex items-center border border-gray-300 rounded-md px-1 relative">
                     <input
                         type="text"
                         placeholder="Search..."
@@ -58,7 +58,20 @@ export default function SearchBar() {
                     <button class="ml-2 p-1  text-white rounded-md cursor-pointer focus:outline-none z-50" onClick={handleSearchClick}>
                         <i class="fas fa-search"></i>
                     </button>
-                </div>
+                </div> */}
+                
+                   
+                    <input
+                        class="bg-transparent border border-gray-400 dark:border-gray-600 rounded-md py-1.5 pl-4 pr-3 w-40 duration-300 focus:ring-1 focus:ring-primary focus:border-primary text-gray-800 dark:text-white placeholder-gray-500 dark:placeholder-gray-400"
+                        placeholder="Search..." type="text"
+                        onChange={handleChange}
+                        onKeyDown={(event) => { if (event.key == "Enter") handleSearchClick() }}
+                        value={input}
+                    />
+                    <button className="absolute right-2 top-1 ml-2 p-1  text-white rounded-md cursor-pointer focus:outline-none z-50" onClick={handleSearchClick}>
+                        <i class="fas fa-search" style={{color:"grey"}}></i>
+                    </button>
+               
             </div>
         </>
     );
